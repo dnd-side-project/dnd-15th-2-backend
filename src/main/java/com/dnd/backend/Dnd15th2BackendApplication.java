ccca836c6122753a5a264db226d6a26820d52ab0
Kim Do Yeon <tkv00@naver.com>
2026-07-24 09:41:19 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/dnd/backend/Dnd15th2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd15th2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd15th2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/backend/Dnd15th2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd15th2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
